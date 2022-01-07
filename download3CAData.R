f7cec3fb811ef5295fcf26dbe881ba5e196eac41
Minoru Nakano <minoru.nakano@gmail.com>
2022-01-07 11:52:30 -0500

    trigger commit

--- download3CAData.R
+++ download3CAData.R
@@ -8,6 +8,7 @@ data <- read.csv("https://github.com/BHKLAB-Pachyderm/download3CAData/raw/main/d
 for(i in 1:nrow(data)) {
   link <- data[i,]$download_link
   name <- data[i,]$disease
+  print(paste(i, "of", nrow(data), name))
   file <- paste0(out_dir, name,".zip", sep = "")
   command <- paste("curl -L",link,">",file)
   system(command)
