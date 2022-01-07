3f5f952c957405f4e911aedcfdde4f7bb9f26b8b
Minoru Nakano <minoru.nakano@gmail.com>
2022-01-07 11:33:26 -0500

    update script

--- download3CAData.R
+++ download3CAData.R
@@ -1,8 +1,14 @@
-library(readr)
-library(tools)
-
-# out_dir <- "/pfs/out/"
-out_dir <- "~/Documents/pfs/download3CAData/"
+out_dir <- "/pfs/out/"
+# out_dir <- "~/Documents/pfs/download3CAData/"
 
 options(timeout=6000)
-data <- read_csv("./download_links.csv")
+data <- read.csv("https://github.com/BHKLAB-Pachyderm/download3CAData/raw/main/download_links.csv")
+
+# download data zip file
+for(i in 1:nrow(data)) {
+  link <- data[i,]$download_link
+  name <- data[i,]$disease
+  file <- paste0(out_dir, name,".zip", sep = "")
+  command <- paste("curl -L",link,">",file)
+  system(command)
+}
